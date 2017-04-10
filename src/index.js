5154243c8106cc883fd6a9358a6f36ce345655f1
nick <crismas90@gmail.com>
2017-04-10 17:11:29 +0300

    create rectangle objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-﻿
+﻿  var rect1 = {
+      left: 0,
+      top: 0,
+      width: 20,
+      height: 20
+  };
+  var rect2 = {
+      left: 10,
+      top: 10,
+      width: 25,
+      height: 25
+  };
+  var a = {
+      x: rect1.left,
+      y: rect1.top,
+      x1: rect1.left + rect1.width,
+      y1: rect1.top + rect1.height
+  };
+  var b = {
+      x: rect2.left,
+      y: rect2.top,
+      x1: rect2.left + rect2.width,
+      y1: rect2.top + rect2.height
+  };
+  var intersects = function areIntersected(a, b) {
+
+  }
+
+
+  console.log(intersects);
